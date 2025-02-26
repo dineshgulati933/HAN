dbd3aeeb56f6a13286c975909aa25a71ccc43725
dineshgulati933 <dineshgulati333@gmail.com>
2025-02-26 15:41:12 -0700

    conf_added

--- app.py
+++ app.py
@@ -26,5 +26,13 @@ def gis_project1():
 def gis_project2():
     return render_template("gis/project2.html")
 
+@app.route("/conf")
+def conf():
+    return render_template("conf.html")
+
+@app.route("/conf/wqw")
+def wqw():
+    return render_template("conf/wqw.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
